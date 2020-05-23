53cbcffb628397b0a3885b1a38dd555378f321a9
joseucarvajal <joseucarvajal@gmail.com>
2020-05-22 20:15:06 -0500

    fire controller working

--- src/fire-controller.js
+++ src/fire-controller.js
@@ -1,8 +1,13 @@
 import { playerId, sendMessage } from "./connection";
+import { showErrorMessage } from "./messages-controller";
 
 const gridElements = document.querySelectorAll('.board-player2 .board-grid-element');
 for (let cell of gridElements) {
     cell.addEventListener('click', (e) => {
+        if(playerId == 0){
+            showErrorMessage('There are not connection stablished yet');
+            return;
+        }
         const y = e.target.getAttribute('y');
         const x = e.target.getAttribute('x');
         sendShoot(x, y);
